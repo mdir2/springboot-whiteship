a1cc844196a3e291d6f06c29c16fc41b9147b493
wook@gaeasoft.co.kr <wook@gaeasoft.co.kr>
2019-11-13 12:55:55 +0900

    내장서버컨트롤

---
+++ web-servers-showcase/src/main/java/me/wook/webserversshowcase/WebServersShowcaseApplication.java
@@ -0,0 +1,13 @@
+package me.wook.webserversshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServersShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServersShowcaseApplication.class, args);
+	}
+
+}
