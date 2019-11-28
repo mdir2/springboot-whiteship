3365d0aeb9281cdfb3fff527a5260ad9f0d6ad89
wook@gaeasoft.co.kr <wook@gaeasoft.co.kr>
2019-11-28 13:00:34 +0900

    정적 리소스 매핑하기

--- spring-boot-mvc/src/main/java/me/wook/springbootmvc/config/WebConfig.java
+++ spring-boot-mvc/src/main/java/me/wook/springbootmvc/config/WebConfig.java
@@ -1,8 +1,22 @@
 package me.wook.springbootmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	/**
+	 * Add handlers to serve static resources such as images, js, and, css
+	 * files from specific locations under web application root, the classpath,
+	 * and others.
+	 *
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/m/**")
+				.addResourceLocations("classpath:/m/")
+				.setCachePeriod(20);
+	}
 }
