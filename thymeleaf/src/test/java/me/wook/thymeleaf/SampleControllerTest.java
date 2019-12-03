6e41f63ce94bfabb1a128d9a4c0dc54a48a435f8
wook@gaeasoft.co.kr <wook@gaeasoft.co.kr>
2019-12-03 12:52:33 +0900

    HtmlUnit 사용해보기

--- thymeleaf/src/test/java/me/wook/thymeleaf/SampleControllerTest.java
+++ thymeleaf/src/test/java/me/wook/thymeleaf/SampleControllerTest.java
@@ -1,38 +1,28 @@
 package me.wook.thymeleaf;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
 	@Autowired
-	MockMvc mockMvc;
+	WebClient webClient;
 
 	@Test
 	public void hello() throws Exception {
-		// 요청 "/hello"
-		// 응답
-		// - 모델 name: wook
-		// - 뷰 이름: hello
-
-		mockMvc.perform(get("/hello"))
-				.andExpect(status().isOk())
-				.andDo(print())
-				.andExpect(view().name("hello"))
-				.andExpect(model().attribute("name", is("wook")))
-				.andExpect(content().string(containsString("wook")));
+		HtmlPage page = webClient.getPage("/hello");
+		HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+		System.out.println(h1.getTextContent());
+		assertThat(h1.getTextContent()).isEqualToIgnoringCase("wook");
 	}
 }
